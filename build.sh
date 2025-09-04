f9d250cbd39537e6d60dad4fa5b37f210305a715
Joseph Ezekiel <theolujay@gmail.com>
2025-09-04 17:45:22 +0100

    chore: rename project to vmlc-backend

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ push=$3
 if [ -n "$version" ]; then
     if [ "$no_cache" == "nc" ]; then
         echo "Building v$version with no cache"
-        docker build -t vmlc-api:$version . --no-cache
+        docker build -t vmlc-backend:$version . --no-cache
     else
         echo "Building v$version with cache"
-        docker build -t vmlc-api:$version .
+        docker build -t vmlc-backend:$version .
     fi
     
     if [ $? -eq 0 ]; then
         echo "Built! Now tagging to repo..."
-        docker tag vmlc-api:$version theolujay/vmlc-api:$version
+        docker tag vmlc-backend:$version theolujay/vmlc-backend:$version
         
         if [ "$push" == "p" ]; then
-            echo "Pushing v$version to repo -> theolujay/vmlc-api"
-            docker push theolujay/vmlc-api:$version
+            echo "Pushing v$version to repo -> theolujay/vmlc-backend"
+            docker push theolujay/vmlc-backend:$version
             echo "Pushed to repo!"
         else
             echo "Not yet pushed to repo"
@@ -30,6 +30,6 @@ if [ -n "$version" ]; then
     fi
 else
     echo "No version provided - building with 'latest'..."
-    docker build -t vmlc-api:latest .
-    echo "Built as 'vmlc-api:latest'"
+    docker build -t vmlc-backend:latest .
+    echo "Built as 'vmlc-backend:latest'"
 fi
