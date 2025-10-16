a377a37113c11dc16172601af353d0d8d33e3ad3
Joseph Ezekiel <theolujay@gmail.com>
2025-10-16 10:56:21 +0100

    return default_from_email

--- vmlc/tasks.py
+++ vmlc/tasks.py
@@ -23,7 +23,7 @@ def send_mail_task(self, subject, message, recipient_list, html_message=None):
         send_mail(
             subject=subject,
             message=message,
-            # from_email=settings.DEFAULT_FROM_EMAIL,
+            from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=recipient_list,
             fail_silently=False,
             html_message=html_message,
