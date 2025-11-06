9f310e3a807350e3abb344f9466435ce35df5e33
Joseph Ezekiel <theolujay@gmail.com>
2025-11-06 03:10:30 +0100

    refactor: generate dashboard upon login and improve auto-email verify for invited users

--- vmlc/signals.py
+++ vmlc/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from .models import User
-from .tasks import send_otp_on_registration_task
+from .tasks import update_candidate_dashboard_cache_task, update_staff_dashboard_cache_task
 
 @receiver(user_logged_in, sender=User)
 def user_logged_in_receiver(sender, request, user, **kwargs):
@@ -9,7 +9,22 @@ def user_logged_in_receiver(sender, request, user, **kwargs):
     Set is_email_verified to True on first login for users who were invited by
     another staff member (that typically has manager or superadmin roles).
     """
-    if not user.is_email_verified:
+    if (
+        hasattr(user, "staff_profile")
+        and user.is_email_verified
+        and user.is_user_verified
+    ):
+        update_staff_dashboard_cache_task.delay(user.id)
+    if (
+        hasattr(user, "candidate_profile")
+        and user.is_email_verified
+        and user.is_user_verified
+    ):
+        update_candidate_dashboard_cache_task.delay(user.id)
+    if (
+        user.last_login is None
+        and not user.is_email_verified
+    ):
         profile = None
         if hasattr(user, "staff_profile"):
             profile = user.staff_profile
