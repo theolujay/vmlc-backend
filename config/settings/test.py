c9818fa928a913a218507cc388186521750278ab
Joseph Ezekiel <theolujay@gmail.com>
2025-12-13 07:58:36 +0100

    build: add daphne before django...staticfiles

--- config/settings/test.py
+++ config/settings/test.py
@@ -9,9 +9,7 @@ from .base import *
 
 SECRET_KEY = "this-is-a-test-secret-key--do-not-use-in-production"
 
-INSTALLED_APPS += [
-    "daphne"
-]
+INSTALLED_APPS.insert(0, "daphne")
 DEBUG = False
 TESTING = True
 INTERNAL_IPS = ["localhost"]
