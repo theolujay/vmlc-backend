b42763d15a06046084dc23196a383dc66c7f0d76
Joseph Ezekiel <theolujay@gmail.com>
2025-12-14 14:12:55 +0100

    fix: load asgi before other components

--- config/asgi.py
+++ config/asgi.py
@@ -11,12 +11,14 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 django.setup()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+# It's important to get the django_asgi_app before importing any other
+# Django components, especially middleware, to ensure all apps are loaded.
+django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from comms.middleware import DualAuthMiddlewareStack
 import comms.routing
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
