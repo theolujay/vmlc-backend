c8e2f13fb17ea0f09fa70015523a910768d0f39b
Joseph Ezekiel <theolujay@gmail.com>
2026-02-26 05:12:05 +0100

    feat: improve CowrywiseKidProfileView to validate username and check candidate enrollment

--- identity/serializers/cowrywise_kid.py
+++ identity/serializers/cowrywise_kid.py
@@ -1,10 +1,7 @@
-
-
-
 from rest_framework import serializers
 
 from identity.models import CowrywiseKidProfile
-from vmlc.utils.exceptions import ValidationError
+
 
 class CowrywiseKidProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,18 +15,7 @@ class CowrywiseKidProfileSerializer(serializers.ModelSerializer):
         read_only_fields = ["candidate", "created_at", "updated_at"]
 
     def create(self, validated_data):
-        request = self.context.get("request")
-        if not request or not hasattr(request.user, "candidate_profile"):
-            return None
+        if "username" in validated_data:
+            validated_data["username"] = validated_data["username"].lower()
 
-        candidate = request.user.candidate_profile
-        username = validated_data.get("username").lower()
-        cowrywise_kid, created = CowrywiseKidProfile.objects.get_or_create(
-            candidate=candidate,
-            username=username,
-            defaults={
-                "candidate": candidate,
-                "username": username,
-            }
-        )
-        return cowrywise_kid
+        return CowrywiseKidProfile.objects.create(**validated_data)
